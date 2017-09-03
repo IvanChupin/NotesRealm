27490a33b87fab72ba2a51331088fc8ce7cff04d
Chupin Ivan <ivan.chupin.95@gmail.com>
2017-09-03 12:59:31 +0300

    Created CustomAdapter in order to change fragments in our viewPager

--- app/src/main/java/com/github/ivanchupin/notesrealm/MainActivity.java
+++ app/src/main/java/com/github/ivanchupin/notesrealm/MainActivity.java
@@ -1,9 +1,17 @@
 package com.github.ivanchupin.notesrealm;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/github/ivanchupin/notesrealm/adapter/CustomAdapter.java
@@ -0,0 +1,21 @@
+package com.github.ivanchupin.notesrealm.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * @author Chupin on 13/08/2017.
+ */
+
+public class CustomAdapter extends FragmentStatePagerAdapter {
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
